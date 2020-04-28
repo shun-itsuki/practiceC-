d89bda2ccfdc982046be096f90299c0b1b115421
shun-itsuki <it_shun_1125@yahoo.co.jp>
2020-04-28 21:47:34 +0900

    2020_02_28

--- practice.cpp
+++ practice.cpp
@@ -1,68 +1,22 @@
-// 4/16 クラス コンストラクタ・デストラクタ
-#include <string.h>
-#include <iostream>
+#include<iostream>
 using namespace std;
-class human{
-  char name[30];
-  int age;
-public:
-  // 下記がコンストラクタ
-  human(char* string, int check);
-  void setname(char* string){
-    strcpy(name, string);
-  }
-  void setage(int check){
-    if (check < 0) {
-      age = 0;
-    }else{
-      age = check;
-    }
-  }
-  char* getname(){
-    return name;
-  }
-  int getage(){
-    return age;
-  }
-  void output();
-};
 
-human::human(char* string, int check){
-  strcpy(name, string);
-  if (check < 0) {
-    age = 0;
-  }else{
-    age = check;
-  }
-  cout << "コンストラクタ呼び出し" << '\n';
+void Kitty() {
+	cout << "Kitty on your lap\n";
 }
 
-// human::~human(){
-//   cout << "デストラクタ呼び出し" << '\n';
-// }
-
-void human::output(){
-  cout << "名前:" << name << endl;
-  cout << "年齢:" << age << endl;
-}
-
-void sub(){
-  cout << "サブです。" << '\n';
-  human tanaka("",25);
-  tanaka.output();
+void Kitty(char *str) {
+	cout << str << '\n';
 }
 
 int main() {
-  // human shun;
-  int n;
-  cout << "main" << '\n';
-  //
-  sub();
-  // shun.setname("");
-  // shun.setage(25);
-  // n = shun.getage();
-  // shun.output();
-  // cout << shun.getname() << shun.getage() << n <<'\n';
-  return 0;
+  // カッコがないとvoid型のポインタと判断されるため、カッコ
+	void (*po1)() , (*po2)(char *);
+	po1 = Kitty;
+	po2 = Kitty;
+
+	(*po1)();
+	(*po2)("Di Gi Gharat");
+	return 0;
 }
 
